63b824406e70541e96e04452391854de57a6acc2
giaphuc757 <giaphuc757@gmail.com>
2022-03-24 23:45:31 +0700

    contact, inf-user

--- contact.php
+++ contact.php
@@ -4,109 +4,113 @@
 <?php include_once("include/cart.php"); ?>
 
 
+
 <!-- Title page -->
 <section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/bg-01.jpg');">
-  <h2 class="ltext-105 cl0 txt-center">
-    Contact
-  </h2>
+    <h2 class="ltext-105 cl0 txt-center">
+        Liên hệ
+    </h2>
 </section>
 
 
 <!-- Content page -->
 <section class="bg0 p-t-104 p-b-116">
-  <div class="container">
-    <div class="flex-w flex-tr">
-      <div class="size-210 bor10 p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
-        <form>
-          <h4 class="mtext-105 cl2 txt-center p-b-30">
-            Send Us A Message
-          </h4>
+    <div class="container">
+        <div class="flex-w flex-tr">
+            <div class="size-210 bor10 p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
+                <form>
+                    <h4 class="mtext-105 cl2 txt-center p-b-30">
+                        Gửi tin nhắn cho chúng tôi
+                    </h4>
 
-          <div class="bor8 m-b-20 how-pos4-parent">
-            <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="email" placeholder="Your Email Address">
-            <img class="how-pos4 pointer-none" src="images/icons/icon-email.png" alt="ICON">
-          </div>
+                    <div class="bor8 m-b-20 how-pos4-parent">
+                        <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="email"
+                            placeholder="Địa chỉ email của bạn">
+                        <img class="how-pos4 pointer-none" src="images/icons/icon-email.png" alt="ICON">
+                    </div>
 
-          <div class="bor8 m-b-30">
-            <textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="msg" placeholder="How Can We Help?"></textarea>
-          </div>
+                    <div class="bor8 m-b-30">
+                        <textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="msg"
+                            placeholder="Bạn cần giúp đỡ về vấn đề gì?"></textarea>
+                    </div>
 
-          <button class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
-            Submit
-          </button>
-        </form>
-      </div>
+                    <button class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
+                        Xác nhận
+                    </button>
+                </form>
+            </div>
 
-      <div class="size-210 bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
-        <div class="flex-w w-full p-b-42">
-          <span class="fs-18 cl5 txt-center size-211">
-            <span class="lnr lnr-map-marker"></span>
-          </span>
+            <div class="size-210 bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
+                <div class="flex-w w-full p-b-42">
+                    <span class="fs-18 cl5 txt-center size-211">
+                        <span class="lnr lnr-map-marker"></span>
+                    </span>
 
-          <div class="size-212 p-t-2">
-            <span class="mtext-110 cl2">
-              Address
-            </span>
+                    <div class="size-212 p-t-2">
+                        <span class="mtext-110 cl2">
+                            Địa chỉ
+                        </span>
 
-            <p class="stext-115 cl6 size-213 p-t-18">
-              Coza Store Center 8th floor, 379 Hudson St, New York, NY 10018 US
-            </p>
-          </div>
+                        <p class="stext-115 cl6 size-213 p-t-18">
+                            Khu Công nghệ cao TP.HCM (SHTP), Xa lộ Hà Nội, P.Hiệp Phú, TP.Thủ Đức, TP.HCM
+                        </p>
+                    </div>
+                </div>
+
+                <div class="flex-w w-full p-b-42">
+                    <span class="fs-18 cl5 txt-center size-211">
+                        <span class="lnr lnr-phone-handset"></span>
+                    </span>
+
+                    <div class="size-212 p-t-2">
+                        <span class="mtext-110 cl2">
+                            Đường dây nóng
+                        </span>
+
+                        <p class="stext-115 cl1 size-213 p-t-18">
+                            +1 800 1236879
+                        </p>
+                    </div>
+                </div>
+
+                <div class="flex-w w-full">
+                    <span class="fs-18 cl5 txt-center size-211">
+                        <span class="lnr lnr-envelope"></span>
+                    </span>
+
+                    <div class="size-212 p-t-2">
+                        <span class="mtext-110 cl2">
+                            Địa chỉ email
+                        </span>
+
+                        <p class="stext-115 cl1 size-213 p-t-18">
+                            contact@gmail.com
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div class="flex-w w-full p-b-42">
-          <span class="fs-18 cl5 txt-center size-211">
-            <span class="lnr lnr-phone-handset"></span>
-          </span>
-
-          <div class="size-212 p-t-2">
-            <span class="mtext-110 cl2">
-              Lets Talk
-            </span>
-
-            <p class="stext-115 cl1 size-213 p-t-18">
-              +1 800 1236879
-            </p>
-          </div>
-        </div>
-
-        <div class="flex-w w-full">
-          <span class="fs-18 cl5 txt-center size-211">
-            <span class="lnr lnr-envelope"></span>
-          </span>
-
-          <div class="size-212 p-t-2">
-            <span class="mtext-110 cl2">
-              Sale Support
-            </span>
-
-            <p class="stext-115 cl1 size-213 p-t-18">
-              contact@example.com
-            </p>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 </section>
 
 
 <!-- Map -->
 <div class="map">
-  <div class="size-303" id="google_map" data-map-x="40.691446" data-map-y="-73.886787" data-pin="images/icons/pin.png" data-scrollwhell="0" data-draggable="1" data-zoom="11"></div>
+    <!-- <div class="size-303" id="google_map" data-map-x="40.691446" data-map-y="-73.886787" data-pin="images/icons/pin.png" data-scrollwhell="0" data-draggable="1" data-zoom="11"></div> -->
+    <iframe class="size-303"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.125120810274!2d106.71230301458925!3d10.801727892304346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528a459cb43ab%3A0x6c3d29d370b52a7e!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2jhu4cgVFAuSENNIC0gSFVURUNI!5e0!3m2!1svi!2s!4v1648094670554!5m2!1svi!2s"
+        width="1688" height="" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 </div>
 
 
-
-<!-- Footer -->
 <?php include_once("include/footer.php"); ?>
 
 
 <!-- Back to top -->
 <div class="btn-back-to-top" id="myBtn">
-  <span class="symbol-btn-back-to-top">
-    <i class="zmdi zmdi-chevron-up"></i>
-  </span>
+    <span class="symbol-btn-back-to-top">
+        <i class="zmdi zmdi-chevron-up"></i>
+    </span>
 </div>
 
 <!--===============================================================================================-->
@@ -119,31 +123,31 @@
 <!--===============================================================================================-->
 <script src="vendor/select2/select2.min.js"></script>
 <script>
-  $(".js-select2").each(function() {
+$(".js-select2").each(function() {
     $(this).select2({
-      minimumResultsForSearch: 20,
-      dropdownParent: $(this).next('.dropDownSelect2')
+        minimumResultsForSearch: 20,
+        dropdownParent: $(this).next('.dropDownSelect2')
     });
-  })
+})
 </script>
 <!--===============================================================================================-->
 <script src="vendor/MagnificPopup/jquery.magnific-popup.min.js"></script>
 <!--===============================================================================================-->
 <script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script>
-  $('.js-pscroll').each(function() {
+$('.js-pscroll').each(function() {
     $(this).css('position', 'relative');
     $(this).css('overflow', 'hidden');
     var ps = new PerfectScrollbar(this, {
-      wheelSpeed: 1,
-      scrollingThreshold: 1000,
-      wheelPropagation: false,
+        wheelSpeed: 1,
+        scrollingThreshold: 1000,
+        wheelPropagation: false,
     });
 
     $(window).on('resize', function() {
-      ps.update();
+        ps.update();
     })
-  });
+});
 </script>
 <!--===============================================================================================-->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKFWBqlKAGCeS1rMVoaNlwyayu0e0YRes"></script>
