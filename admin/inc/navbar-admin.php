e8624fab0d3be693da9b9196a9342a88db86a640
DESKTOP-CCVDSED\phanminhhung <phanminhhung777@gmail.com>
2022-03-25 22:22:15 +0700

    update navbar admin

--- admin/inc/navbar-admin.php
+++ admin/inc/navbar-admin.php
@@ -15,22 +15,30 @@
   </div>
   <div class="main-menu-content">
     <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-      <li class="nav-item">
+    <!-- class="active" -->  
+    <li class="nav-item active">
         <a href="dashboard.php"><i class="ft-home"></i><span class="menu-title" data-i18n="">Dashboard</span></a>
       </li>
-      <li>
-        <!-- class="active" -->
+      <li class="nav-item">
+        <a href="banner.php"><i class="ft-home"></i><span class="menu-title" data-i18n="">Banner</span></a>
+      </li>
+      <li class="nav-item">
+        <a href="category.php"><i class="ft-layers"></i><span class="menu-title" data-i18n="">Danh mục</span></a>
+      </li>
+      <li class="nav-item">
+        <a href="brand.php"><i class="ft-layers"></i><span class="menu-title" data-i18n="">Thương hiệu</span></a>
+      </li>
+      <li class="nav-item">
         <a href="product.php"><i class="ft-layers"></i><span class="menu-title" data-i18n="">Sản phẩm</span></a>
       </li>
       <li class="nav-item">
-        <a href="info.php"><i class="ft-box"></i><span class="menu-title" data-i18n=""></span>Quản lý thông
-          tin</span></a>
+        <a href="employee.php"><i class="ft-box"></i><span class="menu-title" data-i18n="">Nhân viên</span></a>
+      </li>
+      <li class="nav-item">
+        <a href="customer.php"><i class="ft-box"></i><span class="menu-title" data-i18n="">Khách hàng</span></a>
       </li>
       <li class="nav-item">
         <a href="order.php"><i class="ft-bold"></i><span class="menu-title" data-i18n="">Đơn hàng</span></a>
-      </li>
-      <li class="nav-item">
-        <a href="banner.php"><i class="ft-credit-card"></i><span class="menu-title" data-i18n="">Banner</span></a>
       </li>
     </ul>
   </div>
