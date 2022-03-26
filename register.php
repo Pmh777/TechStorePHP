9d4d4ffbb5a4264e19a178646e1a36bd6cd8ebdc
longnt25 <long20002000ht@gmail.com>
2022-03-26 14:46:04 +0700

    Update form signin/signup

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
 <!-- Title page -->
 <section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/bg-01.jpg');">
     <h2 class="ltext-105 cl0 txt-center">
-        Sign In
+        Sign Up
     </h2>
 </section>
 
@@ -18,21 +18,21 @@
         <div class="size-signin bor10 p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
             <form>
                 <h4 class="mtext-105 cl2 txt-center p-b-30">
-                    Sign In
+                    Sign Up
                 </h4>
-
                 <div class="bor8 m-b-20 how-pos4-parent">
-                    <input class="stext-111 cl2 plh3 size-116 p-l-25 p-r-30" type="email" name="email"
-                        placeholder="Email">
+                    <input class="stext-111 cl2 plh3 size-116 p-l-25 p-r-30" type="text" name="Name" placeholder="Name">
+                </div>
+                <div class="bor8 m-b-20 how-pos4-parent">
+                    <input class="stext-111 cl2 plh3 size-116 p-l-25 p-r-30" type="email" name="email" placeholder="Email">
                 </div>
 
                 <div class="bor8 m-b-20 how-pos4-parent">
-                    <input class="stext-111 cl2 plh3 size-116 p-l-25 p-r-30" type="password" name="password"
-                        placeholder="Password">
+                    <input class="stext-111 cl2 plh3 size-116 p-l-25 p-r-30" type="password" name="password" placeholder="Password">
                 </div>
 
                 <button class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
-                    Login
+                    Sign Up
                 </button>
             </form>
         </div>
