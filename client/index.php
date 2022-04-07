ec81a7615cf5fac239fedf9059232aa449234fe3
Duy Truong <truongduy.fu@gmail.com>
2022-04-07 21:58:15 +0700

    update images

--- client/index.php
+++ client/index.php
@@ -14,13 +14,13 @@
           <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
             <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
               <span class="ltext-101 cl2 respon2">
-                Women Collection 2018
+                Sản phẩm độc đáo
               </span>
             </div>
 
             <div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
               <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-                NEW SEASON
+                Chất lượng hàng đầu
               </h2>
             </div>
 
@@ -38,13 +38,13 @@
           <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
             <div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
               <span class="ltext-101 cl2 respon2">
-                Men New-Season
+                Giá cả cạnh tranh
               </span>
             </div>
 
             <div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
               <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-                Jackets & Coats
+                Bảo hành toàn quốc
               </h2>
             </div>
 
@@ -62,13 +62,13 @@
           <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
             <div class="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay="0">
               <span class="ltext-101 cl2 respon2">
-                Men Collection 2018
+                Một đêm duy nhất
               </span>
             </div>
 
             <div class="layer-slick1 animated visible-false" data-appear="rotateInUpRight" data-delay="800">
               <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-                New arrivals
+                Chỉ có tại chúng tôi
               </h2>
             </div>
 
@@ -84,268 +84,103 @@
   </div>
 </section>
 
-<!-- Product -->
-<section class="bg0 p-t-50 p-b-140">
-  <div class="container">
-    <div class="p-b-10 text-center">
-      <h3 class="ltext-103 cl5">
-        Sản phẩm nổi bậc
-      </h3>
-    </div>
+<!--===============================================================================================-->
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/select2/select2.min.js"></script>
+<script>
+$(".js-select2").each(function() {
+  $(this).select2({
+    minimumResultsForSearch: 20,
+    dropdownParent: $(this).next('.dropDownSelect2')
+  });
+})
+</script>
+<!--===============================================================================================-->
+<script src="vendor/daterangepicker/moment.min.js"></script>
+<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/slick/slick.min.js"></script>
+<script src="js/slick-custom.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/parallax100/parallax100.js"></script>
+<script>
+$('.parallax100').parallax100();
+</script>
+<!--===============================================================================================-->
+<script src="vendor/MagnificPopup/jquery.magnific-popup.min.js"></script>
+<script>
+$('.gallery-lb').each(function() { // the containers for all your galleries
+  $(this).magnificPopup({
+    delegate: 'a', // the selector for gallery item
+    type: 'image',
+    gallery: {
+      enabled: true
+    },
+    mainClass: 'mfp-fade'
+  });
+});
+</script>
+<!--===============================================================================================-->
+<script src="vendor/isotope/isotope.pkgd.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/sweetalert/sweetalert.min.js"></script>
+<script>
+$('.js-addwish-b2, .js-addwish-detail').on('click', function(e) {
+  e.preventDefault();
+});
 
-    <div class="flex-w flex-sb-m p-b-52">
-      <div class="flex-w flex-l-m filter-tope-group m-tb-10">
-        <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
-          Tất cả sản phẩm
-        </button>
+$('.js-addwish-b2').each(function() {
+  var nameProduct = $(this).parent().parent().find('.js-name-b2').php();
+  $(this).on('click', function() {
+    swal(nameProduct, "is added to wishlist !", "success");
 
-        <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".phone">
-          Smartphone
-        </button>
+    $(this).addClass('js-addedwish-b2');
+    $(this).off('click');
+  });
+});
 
-        <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".laptop">
-          Laptop
-        </button>
+$('.js-addwish-detail').each(function() {
+  var nameProduct = $(this).parent().parent().parent().find('.js-name-detail').php();
 
-        <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".macbook">
-          Macbook
-        </button>
+  $(this).on('click', function() {
+    swal(nameProduct, "is added to wishlist !", "success");
 
-        <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".accessories">
-          Phụ kiện
-        </button>
-      </div>
+    $(this).addClass('js-addedwish-detail');
+    $(this).off('click');
+  });
+});
 
-      <div class="flex-w flex-c-m m-tb-10">
-        <div class="flex-c-m stext-106 cl6 size-104 bor4 pointer hov-btn3 trans-04 m-r-8 m-tb-4 js-show-filter">
-          <i class="icon-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-filter-list"></i>
-          <i class="icon-close-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-          Bộ lọc
-        </div>
+/*---------------------------------------------*/
 
-        <div class="flex-c-m stext-106 cl6 size-105 bor4 pointer hov-btn3 trans-04 m-tb-4 js-show-search">
-          <i class="icon-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-search"></i>
-          <i class="icon-close-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-          Tìm kiếm
-        </div>
-      </div>
+$('.js-addcart-detail').each(function() {
+  var nameProduct = $(this).parent().parent().parent().parent().find('.js-name-detail').php();
+  $(this).on('click', function() {
+    swal(nameProduct, "is added to cart !", "success");
+  });
+});
+</script>
+<!--===============================================================================================-->
+<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script>
+$('.js-pscroll').each(function() {
+  $(this).css('position', 'relative');
+  $(this).css('overflow', 'hidden');
+  var ps = new PerfectScrollbar(this, {
+    wheelSpeed: 1,
+    scrollingThreshold: 1000,
+    wheelPropagation: false,
+  });
 
-      <!-- Search product -->
-      <div class="dis-none panel-search w-full p-t-10 p-b-15">
-        <div class="bor8 dis-flex p-l-15">
-          <button class="size-113 flex-c-m fs-16 cl2 hov-cl1 trans-04">
-            <i class="zmdi zmdi-search"></i>
-          </button>
-
-          <input class="mtext-107 cl2 size-114 plh2 p-r-15" type="text" name="search-product" placeholder="Tìm kiếm">
-        </div>
-      </div>
-
-      <!-- Filter -->
-      <?php include_once("include/filter.php"); ?>
-    </div>
-
-    <div class="row isotope-grid">
-      <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item phone">
-        <!-- Block2 -->
-        <div class="block2">
-          <div class="block2-pic hov-img0">
-            <img src="images/product/product-02.png" alt="IMG-PRODUCT">
-
-            <a href="#"
-              class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-              Xem nhanh
-            </a>
-          </div>
-
-          <div class="block2-txt flex-w flex-t p-t-14">
-            <div class="block2-txt-child1 flex-col-l ">
-              <a href="product/product-detail.php" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                Tên sản phẩm
-              </a>
-
-              <span class="stext-105 cl3">
-                Giá sản phẩm
-              </span>
-            </div>
-
-
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item phone">
-        <!-- Block2 -->
-        <div class="block2">
-          <div class="block2-pic hov-img0">
-            <img src="images/product/product-03.png" alt="IMG-PRODUCT">
-
-            <a href="#"
-              class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-              Xem nhanh
-            </a>
-          </div>
-
-          <div class="block2-txt flex-w flex-t p-t-14">
-            <div class="block2-txt-child1 flex-col-l ">
-              <a href="product/product-detail.php" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                Tên sản phẩm
-              </a>
-
-              <span class="stext-105 cl3">
-                Giá sản phẩm
-              </span>
-            </div>
-
-
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item phone">
-        <!-- Block2 -->
-        <div class="block2">
-          <div class="block2-pic hov-img0">
-            <img src="images/product/product-04.png" alt="IMG-PRODUCT">
-
-            <a href="#"
-              class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-              Xem nhanh
-            </a>
-          </div>
-
-          <div class="block2-txt flex-w flex-t p-t-14">
-            <div class="block2-txt-child1 flex-col-l ">
-              <a href="product/product-detail.php" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                Tên sản phẩm
-              </a>
-
-              <span class="stext-105 cl3">
-                Giá sản phẩm
-              </span>
-            </div>
-
-
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item phone">
-        <!-- Block2 -->
-        <div class="block2">
-          <div class="block2-pic hov-img0">
-            <img src="images/product/product-05.png" alt="IMG-PRODUCT">
-
-            <a href="#"
-              class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-              Xem nhanh
-            </a>
-          </div>
-
-          <div class="block2-txt flex-w flex-t p-t-14">
-            <div class="block2-txt-child1 flex-col-l ">
-              <a href="product/product-detail.php" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                Tên sản phẩm
-              </a>
-
-              <span class="stext-105 cl3">
-                Giá sản phẩm
-              </span>
-            </div>
-
-
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item phone">
-        <!-- Block2 -->
-        <div class="block2">
-          <div class="block2-pic hov-img0">
-            <img src="images/product-08.png" alt="IMG-PRODUCT">
-
-            <a href="#"
-              class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-              Xem nhanh
-            </a>
-          </div>
-
-          <div class="block2-txt flex-w flex-t p-t-14">
-            <div class="block2-txt-child1 flex-col-l ">
-              <a href="product-detail.php" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                Tên sản phẩm
-              </a>
-
-              <span class="stext-105 cl3">
-                Giá sản phẩm
-              </span>
-            </div>
-
-
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item phone">
-        <!-- Block2 -->
-        <div class="block2">
-          <div class="block2-pic hov-img0">
-            <img src="images/product-07.png" alt="IMG-PRODUCT">
-
-            <a href="#"
-              class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-              Xem nhanh
-            </a>
-          </div>
-
-          <div class="block2-txt flex-w flex-t p-t-14">
-            <div class="block2-txt-child1 flex-col-l ">
-              <a href="product-detail.php" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                Tên sản phẩm
-              </a>
-
-              <span class="stext-105 cl3">
-                Giá sản phẩm
-              </span>
-            </div>
-
-
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item laptop">
-        <!-- Block2 -->
-        <div class="block2">
-          <div class="block2-pic hov-img0">
-            <img src="images/product-01.png" alt="IMG-PRODUCT">
-
-            <a href="#"
-              class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-              Xem nhanh
-            </a>
-          </div>
-
-          <div class="block2-txt flex-w flex-t p-t-14">
-            <div class="block2-txt-child1 flex-col-l ">
-              <a href="product-detail.php" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                Tên sản phẩm
-              </a>
-
-              <span class="stext-105 cl3">
-                Giá sản phẩm
-              </span>
-            </div>
-
-
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Load more -->
-    <div class="flex-c-m flex-w w-full p-t-45">
-      <a href="product.php" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
-        Xem thêm
-      </a>
-    </div>
-  </div>
-</section>
-
-
-<!-- Footer -->
-<?php include_once("include/footer.php"); ?>
+  $(window).on('resize', function() {
+    ps.update();
+  })
+});
+</script>
+<!--===============================================================================================-->
+<script src="js/main.js"></script>
